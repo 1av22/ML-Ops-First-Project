0ecb48bdd69aa3e9772889ab0911222635f75dd2
Aditya Verma <adichichi162@gmail.com>
2024-02-01 00:03:37 +0530

    Changed file structure

--- template.py
+++ template.py
@@ -16,7 +16,7 @@ list_of_files = [
     "src/utils/utils.py",
     "src/utils/__init__.py",
     "src/logger/logging.py",
-    "src/exception/exception.py"
+    "src/exception/exception.py",
     "test/unit/__init__.py",
     "test/unit/integration/__init__.py",
     "init_setup.sh",
